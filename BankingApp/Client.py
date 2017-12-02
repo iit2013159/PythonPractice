fccd7a23d6173eb258820121a04ef80ac23c0205
Abhishek <iit2013159@iiita.ac.in>
2017-12-02 05:42:55 +0530

    Banking version0.2 completed

--- BankingApp/Client.py
+++ BankingApp/Client.py
@@ -29,20 +29,26 @@ def addMoney(account,money):
 
     creditAccount =(newService.accountList.get(account).get("Account"))
     creditAccount.balance += money
-    creditAccount.transaction[r.randint(0,100)] ={
-        "money recieved":money,
-        "totalMoney":creditAccount.balance
+    dict2 ={
+        r.randint(0, 100):{
+            "money recieved": money,
+            "totalMoney": creditAccount.balance
+        }
     }
+    creditAccount.transaction.update(dict2)
     print(creditAccount)
     print(newService.accountList.get(account))
     return 0
 
 def reduceMoney(account,money):
     account.balance -= money
-    account.transaction[r.randint(0, 100)] = {
-        "money sent": money,
-        "totalMoney": account.balance
+    dict2 = {
+        r.randint(0, 100): {
+            "money recieved": money,
+            "totalMoney": account.balance
+        }
     }
+    account.transaction.update(dict2)
 
 def transferMoney():
     fromAccount = int(input("Please enter sender A/C number"))
@@ -63,29 +69,60 @@ def transferMoney():
     if(addMoney(toAccount,money) == 0):
         reduceMoney(debitAccount,money)
         print(newService.accountList.get(fromAccount))
+        print("Transaction Successfull")
 
 
 
 
 def takeInput():
-    return input("Chooose :\n 1.Add Account \n 2. Transfer money\n 3. Debit Money \n 4.Add Money \n5.Exit")
+    return input("Chooose :\n 1.Add Account \n 2. Transfer money\n 3. Debit Money \n 4.Add Money \n5.viewAccount Report\n6.Exit")
 
 
 def clientOption():
     menuOption = takeInput()
-    while(menuOption != "5"):
+    while(menuOption != "6"):
         if(menuOption == "1"):
             addAccount()
             menuOption = takeInput()
+
         elif(menuOption == "2"):
             transferMoney()
             menuOption = takeInput()
+
         elif(menuOption == "3"):
-            print("This feature is under development")
+            fromAccount = int(input("Please enter  A/C number"))
+            if not newService.accountList.get(fromAccount):
+                print('Account doesnot exist')
+                clientOption()
+            debitAccount = (newService.accountList.get(fromAccount).get("Account"))
+
+            money = int(input("How much amount"))
+            bal = newService.accountList.get(fromAccount).get("balance")
+            if (bal < money):
+                print("Low Balance Not possible")
+                clientOption()
+            reduceMoney(debitAccount, money)
+            print("Debit Successfull")
             menuOption = takeInput()
+
         elif (menuOption == "4"):
-            print("This feature is under development")
+            toAccount = int(input("Please enter  A/C number"))
+            if not newService.accountList.get(toAccount):
+                print('Account doesnot exist')
+                clientOption()
+            money = int(input("How much amount"))
+            bal = newService.accountList.get(toAccount).get("balance")
+            if (bal < money):
+                print("Low Balance Not possible")
+                clientOption()
+            addMoney(toAccount, money)
+            print("credit Successfull")
             menuOption = takeInput()
+        elif(menuOption == "5"):
+            someAccount = int(input("Please enter  A/C number"))
+            if not newService.accountList.get(someAccount):
+                print('Account doesnot exist')
+            print(newService.accountList.get(someAccount))
         else:
             print("Not valid choice")
             menuOption = takeInput()
